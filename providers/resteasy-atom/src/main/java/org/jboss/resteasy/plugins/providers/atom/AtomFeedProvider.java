0137fb5866850708450358f775d6a6d5e477fd2b
James R. Perkins <jperkins@redhat.com>
2021-07-07 20:04:23 -0700

    Use javassist to implement the NamespacePrefixMapper. Revert when WFLY-14709 is fixed.

--- providers/resteasy-atom/src/main/java/org/jboss/resteasy/plugins/providers/atom/AtomFeedProvider.java
+++ providers/resteasy-atom/src/main/java/org/jboss/resteasy/plugins/providers/atom/AtomFeedProvider.java
@@ -1,13 +1,5 @@
 package org.jboss.resteasy.plugins.providers.atom;
 
-import org.glassfish.jaxb.runtime.marshaller.NamespacePrefixMapper;
-import org.jboss.resteasy.core.messagebody.AsyncBufferedMessageBodyWriter;
-import org.jboss.resteasy.plugins.providers.jaxb.JAXBContextFinder;
-import org.jboss.resteasy.plugins.providers.jaxb.JAXBMarshalException;
-import org.jboss.resteasy.plugins.providers.jaxb.JAXBUnmarshalException;
-import org.jboss.resteasy.plugins.providers.resteasy_atom.i18n.Messages;
-import org.jboss.resteasy.resteasy_jaxrs.i18n.LogMessages;
-
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
@@ -21,11 +13,21 @@ import jakarta.ws.rs.ext.Providers;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.PropertyException;
+import javassist.util.proxy.MethodHandler;
+import javassist.util.proxy.ProxyFactory;
+import org.jboss.resteasy.core.messagebody.AsyncBufferedMessageBodyWriter;
+import org.jboss.resteasy.plugins.providers.jaxb.JAXBContextFinder;
+import org.jboss.resteasy.plugins.providers.jaxb.JAXBMarshalException;
+import org.jboss.resteasy.plugins.providers.jaxb.JAXBUnmarshalException;
+import org.jboss.resteasy.plugins.providers.resteasy_atom.i18n.Messages;
+import org.jboss.resteasy.resteasy_jaxrs.i18n.LogMessages;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Type;
 import java.util.HashSet;
 
@@ -114,22 +116,62 @@ public class AtomFeedProvider implements MessageBodyReader<Feed>, AsyncBufferedM
       {
          JAXBContext ctx = finder.findCacheContext(mediaType, annotations, set.toArray(new Class[set.size()]));
          Marshaller marshaller = ctx.createMarshaller();
-         NamespacePrefixMapper mapper = new NamespacePrefixMapper()
-         {
-            public String getPreferredPrefix(String namespace, String s1, boolean b)
-            {
-               if (namespace.equals("http://www.w3.org/2005/Atom")) return "atom";
-               else return s1;
-            }
-         };
 
-         marshaller.setProperty("org.glassfish.jaxb.namespacePrefixMapper", mapper);
+         final Object mapper = createMapper();
+         try {
+            marshaller.setProperty("org.glassfish.jaxb.namespacePrefixMapper", mapper);
+         } catch (PropertyException e) {
+            LogMessages.LOGGER.debugf(e, "Failed to set org.glassfish.jaxb.namespacePrefixMapper with %s", mapper);
+         }
+         try {
+            marshaller.setProperty("com.sun.jaxb.namespacePrefixMapper", mapper);
+         } catch (PropertyException e) {
+            LogMessages.LOGGER.debugf(e, "Failed to set com.sun.jaxb.namespacePrefixMapper with %s", mapper);
+         }
 
          marshaller.marshal(feed, entityStream);
       }
       catch (JAXBException e)
       {
          throw new JAXBMarshalException(Messages.MESSAGES.unableToMarshal(mediaType), e);
+      }
+   }
+
+   private Object createMapper() {
+      Class<?> type;
+      try {
+         type = Class.forName("org.glassfish.jaxb.runtime.marshaller.NamespacePrefixMapper");
+      } catch (ClassNotFoundException e) {
+         try {
+            type = Class.forName("com.sun.xml.bind.marshaller.NamespacePrefixMapper");
+         } catch (ClassNotFoundException e2) {
+            throw new RuntimeException("Failed to create a NamespacePrefixMapper. Missing Jakarta XML Binding implementation.");
+         }
+      }
+      final ProxyFactory factory = new ProxyFactory() {
+         @Override
+         protected ClassLoader getClassLoader0() {
+            return AtomFeedProvider.class.getClassLoader();
+         }
+      };
+      factory.setSuperclass(type);
+      factory.setFilter(m -> m.getName().equals("getPreferredPrefix"));
+
+      final MethodHandler handler = (self, thisMethod, proceed, args) -> {
+         if (args != null && args.length > 2) {
+            if ("http://www.w3.org/2005/Atom".equals(args[1])) {
+               return "atom";
+            } else {
+               return args[1];
+            }
+         }
+         return null;
+      };
+
+      try {
+         return factory.create(new Class<?>[0], new Object[0], handler);
+      } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+         throw new RuntimeException("Failed to create a NamespacePrefixMapper", e);
       }
    }
 }
